c4aaeee1eae12314b9f777c25e01146fed4b575d
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-08-16 21:13:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,15 @@
 katz_deli = []
 
 
-def line()
-  line_array = []
-  if x.length == 0  
+def line_method(numinline)
+  line_method_array = []
+  if numinline.length == 0
     puts "The line is currently empty."
-  else 
-    line_array.each_with_index(0) do |name, index| 
-    line_array.push("#{index}. #{name}")
+  else
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
     end
-    puts "The line is currently: #{line_array.join(" ")}."
+    puts "The line is: #{line_method_array.join(" ")}"
   end
 end
 
