0983b18a4a3829e447d77ed70f8ac156a2dd2a6d
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-08-16 21:15:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@
 katz_deli = []
 
 
-def line(numinline)
+def line(katz_deli)
   line_method_array = []
-  if numinline.length == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     line_method_array.each.with_index(0) do |name, index|
