aa389f6dcd27dc77f9eaed48b020de8e2bdb24cd
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-08-16 20:16:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
   else 
     puts katz_deli
     katz_deli.each_with_index do |index, value| 
-      puts "The line is currently: #{index}. #{value}"
+      puts "The line is currently: " + (value) #{value}"
     end
 end
 
