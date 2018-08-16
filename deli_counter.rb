ca54f4f06b71fffdeb31ef40659d792d80bf6943
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-08-16 20:27:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ def line(katz_deli)
   if katz_deli.length == 0  
     puts "The line is currently empty."
   elsif katz_deli.length >= 1
-    katz_deli.each_with_index {|katz_deli, index|} 
+    katz_deli.each_with_index do |katz_deli, index| 
     puts "The line is currently: #{katz_deli} #{index + 1}"
   end
 end
