9b75a6c63ccaef51124c3d7b3991e5aee35400c8
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-08-16 18:39:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
 
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
 
 
